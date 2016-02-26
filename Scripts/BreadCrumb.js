525571025e50e85b863da48647a10de7e35f9300
RajanDuraisamy <d.rajanduraisamy@gmail.com>
2016-02-26 17:47:42 +0530

    Wedding HTML
    Wedding HTML Product description

---
+++ Scripts/BreadCrumb.js
@@ -0,0 +1,23 @@
+﻿function bindEventToNavigation() {
+    $.each($("#navigation_links > li > a"), function (index, element) {
+        $(element).click(function (event) {
+            breadcrumbStateSaver($(this).attr('href'), $(this).text());
+            showBreadCrumb();
+        });
+    });
+}
+
+function showBreadCrumb() {
+    $("#breadcrumb").html(sessionStorage.breadcrumb);
+}
+
+function breadcrumbStateSaver(link, text) {
+    if (typeof (Storage) != "undefined") {
+        if (sessionStorage.breadcrumb) {
+            var breadcrumb = sessionStorage.breadcrumb;
+            sessionStorage.breadcrumb = breadcrumb + " >> <a href='" + link + "'>" + text + "</a>";
+        } else {
+            sessionStorage.breadcrumb = "<a href='" + link + "'>" + text + "</a>";
+        }
+    }
+}
